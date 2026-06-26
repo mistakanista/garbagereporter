d20428f4e0625279211684886275cdaa8b4e014f
gernotschulmeister <gernotschulmeister@gmx.at>
2026-06-26 08:47:10 +0200

    update db schema

--- backend/src/main/java/com/schulmeister/garbagereporter/GarbageReporterApplication.java
+++ backend/src/main/java/com/schulmeister/garbagereporter/GarbageReporterApplication.java
@@ -1,13 +1,13 @@
-package com.schulmeister.barcamp;
+package com.schulmeister.garbagereporter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BarcampApplication {
+public class GarbageReporterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BarcampApplication.class, args);
+        SpringApplication.run(GarbageReporterApplication.class, args);
     }
 
 }
